d229e259ab3fa5000f4429f5b950877d6f607b4f
agata <a.strojewska@gmail.com>
2016-05-09 20:30:51 +0200

    create hibernate configuration and HibernateUtils class

---
+++ persistence/src/main/java/pl/edu/agh/tai/HibernateUtils.java
@@ -0,0 +1,37 @@
+package pl.edu.agh.tai;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtils {
+    private static final SessionFactory ourSessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return ourSessionFactory;
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
